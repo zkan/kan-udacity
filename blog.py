1a1fae1ed762ab0ce5b240a3df0b051a328fb4f4
Kan O <zkan.cs@gmail.com>
2012-05-10 15:55:15 +0700

    add templates

--- blog.py
+++ blog.py
@@ -3,12 +3,31 @@ import template
 import re
 from google.appengine.ext import db
 
-def blog_key(name = 'default'):
-    return db.Key.from_path('blogs', name)
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
 
 COOKIE_RE = re.compile(r'.+=; Path=/')
 def valid_cookie(cookie):
     return cookie and COOKIE_RE.match(cookie)
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    password = db.TextProperty(required = True)
+    email = db.StringProperty(required = False)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
 
 class Post(db.Model):
     subject = db.StringProperty(required = True)
@@ -62,17 +81,89 @@ class NewPost(template.TemplateHandler):
                                         content = content, 
                                         error = error)
 
+class Welcome(template.TemplateHandler):
+    def get(self):
+        user_id = self.request.cookies.get('user_id')
+        key = db.Key.from_path('User', int(user_id))
+        u = db.get(key)
+        username = u.username
+
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else: 
+            self.redirect('/blog/signup')
+
+class SignUp(template.TemplateHandler):
+    def get(self):
+        self.render('signup-form.html')
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+        
+        params = dict(username = username, 
+                      email = email)
+
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+        
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            query = db.GqlQuery("select * from User where username = :username", username = username)
+            username_in_db = query.fetch(1)
+            
+            if not username_in_db:
+                u = User(username = username, 
+                              password = password, 
+                              email = email)
+                u.put()
+
+                user_id = str(u.key().id())
+                header_value = str('user_id=%s; Path=/' % user_id)
+                self.write(header_value)
+#                if valid_cookie(header_value):
+#                    self.response.headers.add_header('Set-Cookie', header_value)
+#                    self.redirect('/blog/welcome')
+#                else:
+#                    self.redirect('/blog/signup')
+            else:
+               params['error_username'] = "That user already exists."
+               self.render('signup-form.html', **params)
+
+class LogIn(template.TemplateHandler):
+    def get(self):
+        pass
+
 class LogOut(template.TemplateHandler):
     def get(self):
-        self.response.headers.add_header("Set-Cookie", "username=kan; Path=/")
+        self.response.headers.add_header("Set-Cookie", "user_id=; Path=/")
         cookie = self.request.cookies.get('username')
 #        if valid_cookie(cookie):
 #            self.write('yes, valid')
-#        self.redirect('/signup')
+        self.redirect('/blog/signup')
 
 app = webapp2.WSGIApplication([('/blog/?', BlogFront),
                                ('/blog/(\d+)', PostPage),
                                ('/blog/newpost', NewPost), 
+                               ('/blog/welcome', Welcome), 
+                               ('/blog/signup', SignUp), 
+                               ('/blog/login', LogIn),
                                ('/blog/logout', LogOut)
                                ], 
                               debug = True)
--- home.py
+++ home.py
@@ -1,33 +1,9 @@
 import webapp2
 
-main_page = """
-<html>
-<head>
-    <title>Kan's Udacity</title>
-</head>
-
-<body>
-
-    Hello, Udacity!
-    <br>
-    <div>
-        <a href='rot13'>rot13</a>
-    </div>
-    <div>
-        <a href='signup'>signup</a>
-    </div>
-    <div>
-        <a href='blog'>blog</a>
-    </div>
-
-</body>
-</html>
-"""
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.out.write(main_page)
+        self.response.out.write('Hello, Udacity!')
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug = True)
 
