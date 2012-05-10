ae6196222077d91d4c4c9dc98ed02d5dd7ec0cc8
Kan O <zkan.cs@gmail.com>
2012-05-10 09:09:58 +0700

    modify templates

--- blog.py
+++ blog.py
@@ -2,53 +2,61 @@ import webapp2
 import template
 from google.appengine.ext import db
 
-class Blog(db.Model):
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
     subject = db.StringProperty(required = True)
     content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
 
-class NewPostHandler(template.TemplateHandler):
-    def render_new_post(self, subject = '', content = '', error = ''):
-        self.render('newpost.html', subject = subject,
-                                    content = content,
-                                    error = error)
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return template.render_str("post.html", p = self)
 
+class NewPost(template.TemplateHandler):
     def get(self):
-        self.render_new_post()
+        self.render("newpost.html")
 
     def post(self):
         subject = self.request.get('subject')
         content = self.request.get('content')
 
         if subject and content:
-            b = Blog(subject = subject, content = content)
-            b.put()
+            p = Post(parent = blog_key(), subject = subject, content = content)
+            p.put()
 
-#            self.write(b.key().id())
-#            t = Blog.get_by_id(b.key().id())
+#            self.write(p.key().id())
+#            t = Post.get_by_id(p.key().id())
 #            self.write(t.subject)
 
-            self.redirect('/blog/' + str(b.key().id()))
+            self.redirect('/blog/' + str(p.key().id()))
         else:
             error = 'subject and content, please!'
-            self.render_new_post(subject, content, error)
+            self.render("newpost.html", subject = subject, 
+                                        content = content, 
+                                        error = error)
 
-class MainPage(template.TemplateHandler):
-    def render_front(self, blog_id = ''):
-        if blog_id:
-            blogs = []
-            blog = Blog.get_by_id(int(blog_id))
-            blogs.append(blog)
-        else:
-            blogs = db.GqlQuery('SELECT * FROM Blog ORDER BY created DESC')
+class BlogFront(template.TemplateHandler):
+    def get(self):
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('front.html', posts = posts)
 
-        self.render('front.html', blogs = blogs) 
+class PostPage(template.TemplateHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
 
-    def get(self, blog_id = ''):
-        self.render_front(blog_id)
+        if not post:
+            self.error(404)
+            return
 
-app = webapp2.WSGIApplication([('/blog/?', MainPage),
-                               ('/blog/(\d+)', MainPage),
-                               ('/blog/newpost', NewPostHandler)], 
-                               debug = True)
+        self.render("permalink.html", post = post)
+
+app = webapp2.WSGIApplication([('/blog/?', BlogFront),
+                               ('/blog/(\d+)', PostPage),
+                               ('/blog/newpost', NewPost)
+                               ], 
+                              debug = True)
 
