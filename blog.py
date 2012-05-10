710adea49d9173466f2b768614e973aa7267be4f
Kan O <zkan.cs@gmail.com>
2012-05-10 11:32:47 +0700

    trying to check if the user exists in db or not

--- blog.py
+++ blog.py
@@ -1,9 +1,14 @@
 import webapp2
 import template
+import re
 from google.appengine.ext import db
 
 def blog_key(name = 'default'):
     return db.Key.from_path('blogs', name)
+
+COOKIE_RE = re.compile(r'.+=; Path=/')
+def valid_cookie(cookie):
+    return cookie and COOKIE_RE.match(cookie)
 
 class Post(db.Model):
     subject = db.StringProperty(required = True)
@@ -15,32 +20,10 @@ class Post(db.Model):
         self._render_text = self.content.replace('\n', '<br>')
         return template.render_str("post.html", p = self)
 
-class NewPost(template.TemplateHandler):
-    def get(self):
-        self.render("newpost.html")
-
-    def post(self):
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-
-        if subject and content:
-            p = Post(parent = blog_key(), subject = subject, content = content)
-            p.put()
-
-#            self.write(p.key().id())
-#            t = Post.get_by_id(p.key().id())
-#            self.write(t.subject)
-
-            self.redirect('/blog/' + str(p.key().id()))
-        else:
-            error = 'subject and content, please!'
-            self.render("newpost.html", subject = subject, 
-                                        content = content, 
-                                        error = error)
-
 class BlogFront(template.TemplateHandler):
     def get(self):
-        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+#        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        posts = Post.all().order('-created')
         self.render('front.html', posts = posts)
 
 class PostPage(template.TemplateHandler):
@@ -54,9 +37,43 @@ class PostPage(template.TemplateHandler):
 
         self.render("permalink.html", post = post)
 
+class NewPost(template.TemplateHandler):
+    def get(self):
+        self.render("newpost.html")
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = Post(parent = blog_key(), 
+                     subject = subject, 
+                     content = content)
+            p.put()
+
+#            self.write(p.key().id())
+#            t = Post.get_by_id(p.key().id())
+#            self.write(t.subject)
+
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = 'subject and content, please!'
+            self.render("newpost.html", subject = subject, 
+                                        content = content, 
+                                        error = error)
+
+class LogOut(template.TemplateHandler):
+    def get(self):
+        self.response.headers.add_header("Set-Cookie", "username=kan; Path=/")
+        cookie = self.request.cookies.get('username')
+#        if valid_cookie(cookie):
+#            self.write('yes, valid')
+#        self.redirect('/signup')
+
 app = webapp2.WSGIApplication([('/blog/?', BlogFront),
                                ('/blog/(\d+)', PostPage),
-                               ('/blog/newpost', NewPost)
+                               ('/blog/newpost', NewPost), 
+                               ('/blog/logout', LogOut)
                                ], 
                               debug = True)
 
--- signup.py
+++ signup.py
@@ -1,158 +1,78 @@
 import webapp2
 import re
+import template
+import user
+from google.appengine.ext import db
 
-signup_page = """
-<!DOCTYPE html>
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
 
-<html>
-<head>
-<title>Sign Up</title>
-    <style type="text/css">
-        .label {text-align: right}
-        .error {color: red}
-    </style>
-</head>
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
 
-<body>
-    <h2>Signup</h2>
-    <form method="post">
-        <table>
-            <tr>
-                <td class="label">
-                    Username
-                </td>
-                <td>
-                    <input type="text" name="username" value="%(username)s">
-                </td>
-                <td class="error">
-                    %(username_error)s
-                </td>
-            </tr>
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
 
-            <tr>
-                <td class="label">
-                    Password
-                </td>
-                <td>
-                    <input type="password" name="password" value="">
-                </td>
-                <td class="error">
-                    %(password_error)s
-                </td>
-            </tr>
-
-            <tr>
-                <td class="label">
-                    Verify Password
-                </td>
-                <td>
-                    <input type="password" name="verify" value="">
-                </td>
-                <td class="error">
-                    %(verify_password_error)s
-                </td>
-            </tr>
-
-            <tr>
-                <td class="label">
-                    Email (optional)
-                </td>
-                <td>
-                    <input type="text" name="email" value="%(email)s">
-                </td>
-                <td class="error">
-                    %(email_error)s
-                </td>
-            </tr>
-        </table>
-
-        <input type="submit">
-    </form>
-    <br>
-    <a href = "/">Back</a>
-</body>
-
-</html>
-"""
-
-class WelcomeHandler(webapp2.RequestHandler):
-    def is_valid_username(self, username):
-        USER_RE = re.compile(r'^[a-zA-Z0-9_-]{3,20}$')
-        if USER_RE.match(username):
-            return True
-
+class Welcome(template.TemplateHandler):
     def get(self):
-        username = self.request.get('username')
-        if not self.is_valid_username(username):
+        username = self.request.cookies.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else: 
             self.redirect('/signup')
 
-        self.response.out.write('Welcome, %s!' % username)
-
-class SignUpHandler(webapp2.RequestHandler):
-    def is_valid_username(self, username):
-        self.USER_RE = re.compile(r'^[a-zA-Z0-9_-]{3,20}$')
-        if not self.USER_RE.match(username):
-            return 'That\'s not a valid username.'
-        else:
-            return ''
-            
-    def is_valid_password(self, password):
-        self.PASS_RE = re.compile(r'^.{3,20}$')
-        if not self.PASS_RE.match(password):
-            return 'That wasn\'t a valid password.'
-        else:
-            return ''
-
-    def is_match_password(self, password, verify):
-        if password != verify:
-            return 'Your passwords didn\'t match.'
-        else:
-            return ''
-
-    def is_valid_email(self, email):
-        if email == '':
-            return ''
-        self.EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
-        if not self.EMAIL_RE.match(email):
-            return 'That\'s not a valid email.'
-        else:
-            return ''
-
-    def write_form(self, username = '', email = '', username_error = '', 
-                   password_error = '', verify_password_error = '', 
-                   email_error = ''):
-        self.response.out.write(signup_page % {'username': username, 
-                                               'email': email, 
-                                               'username_error': username_error, 
-                                               'password_error': password_error,
-                                               'verify_password_error': verify_password_error, 
-                                               'email_error': email_error}) 
-
+class SignUp(template.TemplateHandler):
     def get(self):
-        self.write_form()
+        self.render('signup-form.html')
 
-    def post(self): 
+    def post(self):
+        have_error = False
         username = self.request.get('username')
         password = self.request.get('password')
         verify = self.request.get('verify')
         email = self.request.get('email')
+        
+        params = dict(username = username, 
+                      email = email)
 
-        username_error = self.is_valid_username(username)
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
 
-        password_error = self.is_valid_password(password)
-        verify_password_error = ''
-        if password_error == '':
-            verify_password_error = self.is_match_password(password, verify)
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
 
-        email_error = self.is_valid_email(email)
-
-        if username_error == '' and password_error == '' and verify_password_error == '' and email_error == '':
-            self.redirect('/welcome?username=%s' % username)
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+        
+        if have_error:
+            self.render('signup-form.html', **params)
         else:
-            self.write_form(username, email, username_error, password_error, 
-                            verify_password_error, email_error)
+            username_in_db = db.GqlQuery("select * from User where username = :username", username)
+            
+            self.write(username)
+#            if not username_in_db.get():
+#                u = user.User(username = username, 
+#                              password = password, 
+#                              email = email)
+#                u.put()
+#                header_value = str('username=%s; Path=/' % username)
+#                self.response.headers.add_header('Set-Cookie', header_value)
+#                self.redirect('/welcome')
+#            else:
+#               params['error_username'] = "That user already exists."
+#               self.render('signup-form.html', **params)
 
-app = webapp2.WSGIApplication([('/signup', SignUpHandler), 
-                               ('/welcome', WelcomeHandler)], 
-                               debug = True)
+app = webapp2.WSGIApplication([('/signup', SignUp), 
+                               ('/welcome', Welcome)
+                               ], 
+                              debug = True)
 
